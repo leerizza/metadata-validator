7e9986efc7a9d3c3086df7437d0716d11b110399
Riza Gumelar <207746@bfi.co.id>
2025-10-24 15:42:05 +0700

    test 99999

---
+++ deploy/stg_git_friday.sql
@@ -0,0 +1,8 @@
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[stg_git_friday]') AND type = 'U')
+BEGIN
+  CREATE TABLE [dbo].[stg_git_friday (
+    id INT NOT NULL PRIMARY KEY,
+    branch_name VARCHAR(200) NOT NULL,
+    user_name VARCHAR(100) NULL
+  );
+END;
