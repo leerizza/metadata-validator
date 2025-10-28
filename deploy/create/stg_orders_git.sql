9077420e482b92fb80c72965cf2b192cb309bae8
Riza Gumelar <207746@bfi.co.id>
2025-10-28 14:49:42 +0700

    test PR modular

---
+++ deploy/create/stg_orders_git.sql
@@ -0,0 +1,10 @@
+IF OBJECT_ID(N'[dbo].[stg_orders_git]','U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[stg_orders_git](
+        order_id INT NOT NULL,
+        order_date DATETIME2 NOT NULL,
+        customer_name VARCHAR(100) NOT NULL,
+        amount DECIMAL(18,2) NOT NULL,
+        CONSTRAINT PK_stg_orders_git PRIMARY KEY CLUSTERED (order_id)
+    );
+END;
