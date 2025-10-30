16c54ee76193d9f63dbae6eb2436035fdcfec035
Riza Gumelar <207746@bfi.co.id>
2025-10-30 16:27:29 +0700

    alter column name and length

--- deploy/INC212/002_alter_stg_test_table.sql
+++ deploy/INC212/002_alter_stg_test_table.sql
@@ -1,3 +1,8 @@
 
-ALTER TABLE dbo.stg_test_table
-ALTER COLUMN full_name VARCHAR(MAX);
+-- ALTER TABLE dbo.stg_test_table
+-- ALTER COLUMN full_name VARCHAR(MAX);
+
+ALTER TABLE dbo.stg_test_table 
+ALTER COLUMN full_name VARCHAR(200);
+
+EXEC sp_rename 'dbo.stg_test_table.full_name', 'FullName', 'COLUMN';
