3c961993317bc112682a87040c19dc7f8142a65b
Riza Gumelar <207746@bfi.co.id>
2025-11-04 17:02:19 +0700

    test add column

--- deploy/INC213/Add_Column_DB_NDM.sql
+++ deploy/INC213/Add_Column_DB_NDM.sql
@@ -1,59 +1,16 @@
 --DB_NDM
-ALTER TABLE DatamartDashboardSurveyBravo ADD CancelDate DATETIME NUll;
-ALTER TABLE DatamartDashboardSurveyBravo ADD UnderwritingReturnCumulative INT NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD TimestampUnderwriting DATETIME NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD ApproveCancelDate DATETIME NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD SLAProspectToAssign  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD SLAProspectToAssigninSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD SLA_InputToApprove  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD SLA_InputToApproveinSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD SLA_ApprovalDateToGoliveDate varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD SLAApprovetoGoliveinSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD BranchInputTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD BranchSurveyTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD BranchBookingTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD RescoringSurveyor INT NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ADD RescoringOperation INT NULL;
-
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD CancelDate DATETIME NUll;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD UnderwritingReturnCumulative INT NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD TimestampUnderwriting DATETIME NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD ApproveCancelDate DATETIME NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD SLAProspectToAssign  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD SLAProspectToAssigninSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD SLA_InputToApprove varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD SLA_InputToApproveinSecond numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD SLA_ApprovalDateToGoliveDate  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD SLAApprovetoGoliveinSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD BranchInputTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD BranchSurveyTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD BranchBookingTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD RescoringSurveyor INT NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ADD RescoringOperation INT NULL;
-
-
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD CancelDate DATETIME NUll;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD UnderwritingReturnCumulative INT NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD TimestampUnderwriting DATETIME NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD ApproveCancelDate DATETIME NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD SLAProspectToAssign  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD SLAProspectToAssigninSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD SLA_InputToApprove  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD SLA_InputToApproveinSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD SLA_ApprovalDateToGoliveDate  varchar(36) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD SLAApprovetoGoliveinSecond  numeric(17,2) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD BranchInputTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD BranchSurveyTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD BranchBookingTimeZone varchar(5) NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD RescoringSurveyor INT NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ADD RescoringOperation INT NULL;
-
-
-ALTER TABLE DatamartDashboardSurveyBravo ALTER COLUMN golivedate datetime NULL;
-ALTER TABLE DatamartDashboardSurveyBravo ALTER COLUMN NewAppDate datetime NULL;
-
-ALTER TABLE DatamartDashboardSurveyBravoHistory ALTER COLUMN golivedate datetime NULL;
-ALTER TABLE DatamartDashboardSurveyBravoHistory ALTER COLUMN NewAppDate datetime NULL;
-
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ALTER COLUMN golivedate datetime NULL;
-ALTER TABLE DatamartDashboardSurveyBravo2Monthly ALTER COLUMN NewAppDate datetime NULL;
+ALTER TABLE stg_test_table ADD CancelDate DATETIME NUll;
+ALTER TABLE stg_test_table ADD UnderwritingReturnCumulative INT NULL;
+ALTER TABLE stg_test_table ADD TimestampUnderwriting DATETIME NULL;
+ALTER TABLE stg_test_table ADD ApproveCancelDate DATETIME NULL;
+ALTER TABLE stg_test_table ADD SLAProspectToAssign  varchar(36) NULL;
+ALTER TABLE stg_test_table ADD SLAProspectToAssigninSecond  numeric(17,2) NULL;
+ALTER TABLE stg_test_table ADD SLA_InputToApprove  varchar(36) NULL;
+ALTER TABLE stg_test_table ADD SLA_InputToApproveinSecond  numeric(17,2) NULL;
+ALTER TABLE stg_test_table ADD SLA_ApprovalDateToGoliveDate varchar(36) NULL;
+ALTER TABLE stg_test_table ADD SLAApprovetoGoliveinSecond  numeric(17,2) NULL;
+ALTER TABLE stg_test_table ADD BranchInputTimeZone varchar(5) NULL;
+ALTER TABLE stg_test_table ADD BranchSurveyTimeZone varchar(5) NULL;
+ALTER TABLE stg_test_table ADD BranchBookingTimeZone varchar(5) NULL;
+ALTER TABLE stg_test_table ADD RescoringSurveyor INT NULL;
+ALTER TABLE stg_test_table ADD RescoringOperation INT NULL;
