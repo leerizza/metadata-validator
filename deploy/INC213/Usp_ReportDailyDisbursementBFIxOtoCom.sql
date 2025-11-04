67ee159bb15fd09bb623942a88835f84b580bd5e
Riza Gumelar <207746@bfi.co.id>
2025-11-04 15:16:06 +0700

    test positive

--- deploy/INC213/Usp_ReportDailyDisbursementBFIxOtoCom.sql
+++ deploy/INC213/Usp_ReportDailyDisbursementBFIxOtoCom.sql
@@ -600,7 +600,7 @@ LEFT join (
 IF (1 = 0)
 BEGIN
     -- 1) Trigger ALTER MAX-type (error)
-    ALTER TABLE db_temp..Temp_MainDailyDisbursement ADD DebugCol VARCHAR(MAX);
+    ALTER TABLE db_temp..Temp_MainDailyDisbursement ADD debug_col VARCHAR(100);
 
     -- 2) Trigger TRUNCATE TABLE (security/data-loss)
     TRUNCATE TABLE db_temp..Temp_MainDailyDisbursement;
